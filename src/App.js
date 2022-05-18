ab1d0e32a38ec87849a83068c0f5f72e8d1d4e5c
karthik <karthikj.ele@gmail.com>
2022-05-18 02:09:00 -0700

    Accordin React js

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import React, { useState } from "react";
+import Data from "./Data";
+import Accordion from "./Accordion";
+
+
+const App = () => {
+
+    const [data, setData] = useState(Data);
+    return (
+        <>
+
+            <h1>React Accordion</h1>
+            <div className="container">
+
+                <div className="accordionBlock">
+
+                    {Data.map((val) => {
+                        return (
+                            <>
+                            <Accordion {...val}/>
+                              
+                            </>
+                        )
+                    })}
+
+
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,29 @@
+const Data=[
+    {
+        id:1,
+        question:"what is React?",
+        answer:"React is javascript library for creating user interfaces"
+    },
+    {
+        id:2,
+        question:"who Invented React?",
+        answer:"React is javascript library for creating user interfaces"
+    },
+    {
+        id:3,
+        question:"what is Jsx?",
+        answer:"React is javascript library for creating user interfaces"
+    },
+    {
+        id:4,
+        question:"what is features React",
+        answer:"React is javascript library for creating user interfaces"
+    },
+    {
+        id:5,
+        question:"what is Javascript?",
+        answer:"React is javascript library for creating user interfaces"
+    }
+]
+
+export default Data;
